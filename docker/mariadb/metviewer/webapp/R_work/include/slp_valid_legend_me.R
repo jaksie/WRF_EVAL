039bc07c024e7c510c8ab68b5438c7331ff21b50
jaksie <northernliber@gmail.com>
2026-09-08 22:12:03 +0200

    fix: change ylabels for ME panel

--- docker/mariadb/metviewer/webapp/R_work/include/slp_valid_legend_me.R
+++ docker/mariadb/metviewer/webapp/R_work/include/slp_valid_legend_me.R
@@ -51,7 +51,7 @@ local({
     par(xpd=xp);
     lines(listX,me,type="b",pch=20,col="#8000ffFF");
     axis(1,at=listX[seq(1,length(listX),2)],labels=FALSE,tcl=.35);
-    axis(2,at=yt,las=1,tcl=.35);
+    axis(2,at=yt,las=1,padj=.5,hadj=.5,cex.axis=1,tcl=.35);
     box();
-    mtext("ME [hPa]",side=2,line=2.8,cex=.9,las=0);
+    mtext("ME [hPa]",side=2,line=1,padj=-2,adj=.5,cex=1,font=1,las=0);
 })
